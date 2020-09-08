075fafa8688e17bf9129e6f855e066464d70a4fb
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-09-08 11:37:04 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
   resources :users, only: [:edit, :update]  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :rooms, only: [:new, :create]
 end
