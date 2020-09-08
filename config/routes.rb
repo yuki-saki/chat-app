69ae536ebd52493e00aa7f17c77463711b17a603
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-09-08 18:51:49 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'messages/index'
   devise_for :users
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
