a76cd36ca65581720438c4276723f550b7bb3a55
next-bright-rich <yukiko.sakimura.0419@icloud.com>
2020-09-09 12:42:05 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,3 +2,7 @@ class MessagesController < ApplicationController
   def index
   end
 end
+
+def message_params
+  params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+end
